c2fcd19ad678e9dc8a529bc86ba1a8ccdc5640a5
Marcelo Soares <marcelohsoares142@gmail.com>
2024-09-21 19:41:26 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,44 @@
+AOS.init();
+
+const dataDoEvento = new Date("Sep 26, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diasEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMS = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diasEmMs) / horaEmMS
+  );
+
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % horaEmMS) / minutoEmMs
+  );
+
+  const segundosAteOEvento = Math.floor(
+    (distanciaAteOEvento % minutoEmMs) / 1000
+  );
+
+  /* console.log(diasAteOEvento);
+  console.log(horasAteOEvento);
+  console.log(minutosAteOEvento);
+  console.log(segundosAteOEvento); */
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementsByClassName("hero__text")[0].innerHTML =
+      "O Evento de tecnologia foi expirado";
+  }
+}, 1000);
